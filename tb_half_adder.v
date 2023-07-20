6ff987106065df2082b5188ea07257b5fcb54ef6
Gunavant <139606020+gunavant-s@users.noreply.github.com>
2023-07-20 08:06:58 +0530

    Create tb_half_adder.v

---
+++ tb_half_adder.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Gunavant 
+// 
+// Create Date: 20.07.2023 07:59:12
+// Design Name:Gunavant 
+// Module Name: tb_half_adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_half_adder;
+    reg a,b;
+    wire sum,carry;
+    
+    half_adder HA(.a(a),.b(b),.carry(carry),.sum(sum));
+    
+    initial begin
+        #5; a = 0; b = 0;
+        #5; a = 0; b = 1;
+        #5; a = 1; b = 0;
+        #5; a = 1; b = 1;
+        $stop;
+    end 
+endmodule
